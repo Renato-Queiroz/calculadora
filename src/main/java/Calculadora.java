0259acef1aa0b60d4962ac1933453be6c485e974
Renato-Queiroz <fcorenato.queiroz@gmail.com>
2021-01-22 01:42:02 -0300

    alteracoes solicitadas issue-3

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.lang.ref.Cleaner;
 import java.util.Scanner;
 
 
@@ -7,7 +6,6 @@ public class Calculadora {
 
     public static void main(String[] args) throws IOException, InterruptedException {
         Scanner scanner = new Scanner(System.in);
-
         System.out.print("#############################\n" + "#### CALCULADORA MONSTRA #### \n" + "#############################\n");
         System.out.println("- Escolha uma das Operações abaixo listadas ou digite [X] para sair.");
         System.out.print("==========\n" + "OPERAÇÕES:\n" + "==========");
@@ -17,51 +15,22 @@ public class Calculadora {
         String opcao = scanner.next();
         opcao = opcao.toUpperCase();
         while (opcao.charAt(0) != 'X') {
-            if (opcao.charAt(0) == 'A') {
-                ProcessBuilder processBuilder = new ProcessBuilder();
-                new ProcessBuilder("bash", "-c", "clear").inheritIO().start().waitFor();
-                System.out.println("A OPÇÃO SELECIONADA FOI A: " + opcao.charAt(0));
-                break;
-            } else if (opcao.charAt(0) == 'B') {
-                ProcessBuilder processBuilder = new ProcessBuilder();
-                new ProcessBuilder("bash", "-c", "clear").inheritIO().start().waitFor();
-                System.out.println("A OPÇÃO SELECIONADA FOI A: " + opcao.charAt(0));
-                break;
-            } else if (opcao.charAt(0) == 'C') {
-                ProcessBuilder processBuilder = new ProcessBuilder();
-                new ProcessBuilder("bash", "-c", "clear").inheritIO().start().waitFor();
-                System.out.println("A OPÇÃO SELECIONADA FOI A: " + opcao.charAt(0));
-                break;
-            } else if (opcao.charAt(0) == 'D') {
-                ProcessBuilder processBuilder = new ProcessBuilder();
-                new ProcessBuilder("bash", "-c", "clear").inheritIO().start().waitFor();
-                System.out.println("A OPÇÃO SELECIONADA FOI A: " + opcao.charAt(0));
-                break;
-            } else if (opcao.charAt(0) == 'E') {
-                ProcessBuilder processBuilder = new ProcessBuilder();
-                new ProcessBuilder("bash", "-c", "clear").inheritIO().start().waitFor();
-                System.out.println("A OPÇÃO SELECIONADA FOI A: " + opcao.charAt(0));
-                break;
-            } else if (opcao.charAt(0) == 'F') {
-                ProcessBuilder processBuilder = new ProcessBuilder();
-                new ProcessBuilder("bash", "-c", "clear").inheritIO().start().waitFor();
-                System.out.println("A OPÇÃO SELECIONADA FOI A: " + opcao.charAt(0));
-                break;
-            } else if (opcao.charAt(0) == 'G') {
-                ProcessBuilder processBuilder = new ProcessBuilder();
-                new ProcessBuilder("bash", "-c", "clear").inheritIO().start().waitFor();
-                System.out.println("A OPÇÃO SELECIONADA FOI A: " + opcao.charAt(0));
-                break;
-            } else if (opcao.charAt(0) == 'H') {
-                ProcessBuilder processBuilder = new ProcessBuilder();
-                new ProcessBuilder("bash", "-c", "clear").inheritIO().start().waitFor();
-                System.out.println("A OPÇÃO SELECIONADA FOI A: " + opcao.charAt(0));
-                break;
-            } else {
-                System.out.print("OPÇÃO INVÁLIDA, POR FAVOR DIGITE NOVAMENTE: ");
-                opcao = scanner.next();
-                opcao = opcao.toUpperCase();
+            switch (opcao.charAt(0)) {
+                case 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H' -> {
+                    apagaMenu();
+                    System.out.println("A OPÇÃO SELECIONADA FOI A: " + opcao.charAt(0));
+                    return;
+                }
+                default -> {
+                    System.out.print("OPÇÃO INVÁLIDA, POR FAVOR DIGITE NOVAMENTE: ");
+                    opcao = scanner.next();
+                    opcao = opcao.toUpperCase();
+                }
             }
         }
+    }
+    private static void apagaMenu() throws IOException, InterruptedException {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        new ProcessBuilder("bash", "-c", "clear").inheritIO().start().waitFor();
     }
 }
