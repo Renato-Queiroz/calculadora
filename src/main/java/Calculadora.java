5adc14473df7544770de938b9de6b243926a09eb
Charles Queiroz <charlesqueiroz.redhat@gmail.com>
2021-01-28 17:27:05 -0300

    adicionando metodos de operacoes matematicas 3 / issue-3

--- src/main/java/Calculadora.java
+++ src/main/java/Calculadora.java
@@ -12,8 +12,8 @@ public class Calculadora {
         System.out.println("- Escolha uma das Operações abaixo listadas ou digite [X] para sair.");
         System.out.print("==========\n" + "OPERAÇÕES:\n" + "==========");
         System.out.print("\n" + "[A] - ADIÇÃO \n" + "[B] - SUBTRAÇÃO\n" + "[C] - MULTIPLICAÇÃO\n" + "[D] - DIVISÃO\n" +
-                "[E] - RAIZ QUADRADA\n" + "[F] - POTÊNCIA DE N\n" + "[G] - NUMERO FATORIAL\n" + "[H] - LOGARITIMO BASE 10\n" +
-                "[I] - AREA DE UM POLIGONO\n" + "[J] - CALCULAR A HIPOTENUSA\n" + "\n" + "OPÇÃO ESCOLHIDA: ");
+                "[E] - RAIZ QUADRADA\n" + "[F] - POTÊNCIA DE N\n" + "[G] - NÚMERO FATORIAL\n" + "[H] - LOGARITIMO BASE 10\n" +
+                "[I] - AREA DE UM POLÍGONO REGULAR\n" + "[J] - CALCULAR A HIPOTENUSA DE UM TRIÂNGULO RETÂNGULO\n" + "\n" + "OPÇÃO ESCOLHIDA: ");
         char opcao;
         do {
             opcao = scanner.next().toUpperCase().charAt(0);
@@ -59,21 +59,25 @@ public class Calculadora {
                 case 'G' -> {
                     apagaMenu();
                     System.out.println("A OPÇÃO SELECIONADA FOI A: " + opcao);
+                    fatorialDeUmNumero();
                     return;
                 }
                 case 'H' -> {
                     apagaMenu();
                     System.out.println("A OPÇÃO SELECIONADA FOI A: " + opcao);
+                    logaritmoBaseDez();
                     return;
                 }
                 case 'I' -> {
                     apagaMenu();
                     System.out.println("A OPÇÃO SELECIONADA FOI A: " + opcao);
+                    areaDeUmPoligonoRegular();
                     return;
                 }
                 case 'J' -> {
                     apagaMenu();
                     System.out.println("A OPÇÃO SELECIONADA FOI A: " + opcao);
+                    hipotenusaDeUmTrianguloRetangulo();
                     return;
                 }
                 default -> System.out.print("OPÇÃO INVÁLIDA, POR FAVOR DIGITE NOVAMENTE: ");
@@ -129,19 +133,66 @@ public class Calculadora {
         Scanner scanner = new Scanner(System.in);
         scanner.useLocale(Locale.US);
         System.out.print("Digite um numero: ");
-        double num1 =scanner.nextDouble();
+        double num1 = scanner.nextDouble();
         num1 = Math.sqrt(num1);
         BigDecimal result = BigDecimal.valueOf(num1);
         System.out.println("A raiz quadrada do número é: " + result);
     }
+
     private static void potenciaDeUmNumero() {
         Scanner scanner = new Scanner(System.in);
         scanner.useLocale(Locale.US);
         System.out.print("Digite o numero da base: ");
         double num1 = scanner.nextDouble();
         System.out.print("Digite o numero do expoente: ");
-        double num2 =scanner.nextDouble();
+        double num2 = scanner.nextDouble();
         double result = Math.pow(num1, num2);
         System.out.println("A potencia do número é: " + result);
+    }
+
+    private static void fatorialDeUmNumero() {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+        System.out.print("Digite um numero: ");
+        double x = scanner.nextInt();
+        double f = x;
+        while (x > 1) {
+            f = f * (x - 1);
+            x--;
+        }
+        System.out.println(f);
+    }
+
+    private static void logaritmoBaseDez() {
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+        System.out.print("Digite um numero: ");
+        double num = scanner.nextDouble();
+        num = Math.log10(num);
+        System.out.println(num);
+    }
+
+    private static void areaDeUmPoligonoRegular() {
+        // formula para calculo de um poligono regular: 1/2 * perimetro * apótema.
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+        double constante = 0.5;
+        System.out.print("Digite o valor do perímetro: ");
+        double perimetro = scanner.nextDouble();
+        System.out.print("Digite o valor da apótema: ");
+        double apotema = scanner.nextDouble();
+        System.out.println("A área do polígono regular é: " + BigDecimal.valueOf(constante * perimetro * apotema));
+    }
+
+    private static void hipotenusaDeUmTrianguloRetangulo() {
+        // formula matemática: h² = a² + b².
+        Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
+        System.out.print("Digite o valor do cateto a: ");
+        double catetoA = scanner.nextDouble();
+        System.out.print("Digite o valor do cateto b: ");
+        double catetoB = scanner.nextDouble();
+        double somaQuadradoCatetos = Math.pow(catetoA, 2) + Math.pow(catetoB, 2);
+        System.out.println("A hipotenusa do triângulo retângulo é: " + Math.sqrt(somaQuadradoCatetos));
     }
 }
